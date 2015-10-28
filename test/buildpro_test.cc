1ba9737b3a34e4dbb46c573803a5a414f0707607
ovidiugabriel <ovidiugabriel@gmail.com>
2015-10-28 17:01:08 +0200

    Update buildpro_test.cc

--- test/buildpro_test.cc
+++ test/buildpro_test.cc
@@ -1,7 +1,10 @@
 /* A simple file just to test buildpro. */
 
+#include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf("%s\n", "buildpro_test.cc");
     return 0;
 }
+
