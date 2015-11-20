9d684b8fc95c385de70e2cc4edba0137aa0490ae
ovidiugabriel <ovidiugabriel@gmail.com>
2015-11-20 16:59:48 +0200

    Create emscripten.py

---
+++ src/compiler/emscripten.py
@@ -0,0 +1,6 @@
+#
+# Emscripten is an LLVM-based project that compiles C and C++ into highly-optimizable JavaScript in asm.js format. 
+# This lets you run C and C++ on the web at near-native speed, without plugins.
+#
+# https://kripken.github.io/emscripten-site/
+#
