9db4ec56526b9d7f445510d7ac5c714e817908a4
ovidiugabriel <ovidiugabriel@gmail.com>
2015-11-20 17:15:02 +0200

    Update haxe.py

--- src/compiler/haxe.py
+++ src/compiler/haxe.py
@@ -1,3 +1,7 @@
 #
+# Haxe is an open source toolkit based on a modern, high level, strictly typed programming language, 
+# a cross-compiler, a complete cross-platform standard library and ways to access each platform's 
+# native capabilities. 
+#
 # http://haxe.org
 #
