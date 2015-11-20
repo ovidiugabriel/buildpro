dd8e6162210a0daa62d2d2efbb35893af5c2f95d
ovidiugabriel <ovidiugabriel@gmail.com>
2015-11-20 17:00:49 +0200

    Update coffee.py

--- src/compiler/coffee.py
+++ src/compiler/coffee.py
@@ -1,1 +1,6 @@
+# CoffeeScript is a little language that compiles into JavaScript. 
+# Underneath that awkward Java-esque patina, JavaScript has always had a gorgeous heart. 
+# CoffeeScript is an attempt to expose the good parts of JavaScript in a simple way. 
+#
 # http://coffeescript.org/
+#
