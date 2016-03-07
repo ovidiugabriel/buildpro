526b9f72aa385761149ce9451acac8c03578d87b
ovidiugabriel <ovidiugabriel@gmail.com>
2016-03-07 09:36:16 +0200

    Update base.py

--- src/compiler/base.py
+++ src/compiler/base.py
@@ -11,28 +11,21 @@ class compiler_base:
 
     def append_include_path(self, value):
         self.include_paths.append(value)
-        pass
-    
+
     def append_define(self, key, value):
         self.defines.append([key, value])
-        pass
-    
+
     def append_source(self, value):
         self.sources.append(value)
-        pass
-    
+
     def append_library_path(self, value):
         self.library_paths.append(value)
-        pass
-    
+
     def append_library(self, value):
         self.libraries.append(value)
-        pass
 
     def set_output_artifact(self, tyipe, name):
         self.output = name;
-        pass
-    
+
     def set_logfile(self, tyipe, name):
         self.logfile[tyipe] = name
-        pass
