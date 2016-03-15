48a630a71fc37ddd2438a7c3f05da02f709f1251
ovidiugabriel <ovidiugabriel@gmail.com>
2016-03-15 15:18:38 +0200

    Update run_gcc.sh

--- preprocessor/run_gcc.sh
+++ preprocessor/run_gcc.sh
@@ -3,7 +3,7 @@
 export INCLUDE_PATH=`pwd`/lib
 if [ "$1" != "" ] ; then
   if [ -e $1 ] ; then
-    ./cpp_extension $1
+    php ./cpp_extension.php $1
     gcc -xc output/$1.out
   else
     echo "error: $1: No such file or directory"
