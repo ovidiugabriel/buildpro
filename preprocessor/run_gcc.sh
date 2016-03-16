aa028053126262642bf4ed87e3458f3f80eb3418
ovidiugabriel <ovidiugabriel@gmail.com>
2016-03-16 17:12:20 +0200

    Update run_gcc.sh

--- preprocessor/run_gcc.sh
+++ preprocessor/run_gcc.sh
@@ -3,9 +3,15 @@
 export INCLUDE_PATH=`pwd`/lib
 if [ "$1" != "" ] ; then
   if [ -e $1 ] ; then
+    if [ -e ./output/a.out ] ; then
+      rm ./output/a.out
+    fi
     ./cpp_extension $1
     if [ -e "output/$1.out" ] ; then
       gcc -I./lib -xc output/$1.out -o output/a.out
+      if [ -e ./output/a.out ] ; then
+        ./output/a.out
+      fi
     fi
   else
     echo "error: $1: No such file or directory"
