90144a794478d3c287c703dc23388fadbeb6f710
ovidiugabriel <ovidiugabriel@gmail.com>
2016-03-17 11:39:21 +0200

    Update Preprocessor.class.php

--- preprocessor/Preprocessor.class.php
+++ preprocessor/Preprocessor.class.php
@@ -9,7 +9,20 @@ class Preprocessor {
         $this->INPUT = $INPUT;
     }
 
+    /**
+     * Emits a preprocessing error in the output file.
+     *
+     * @param integer $n_tabs
+     * @param string $text
+     * @return void
+     */
     public function error($n_tabs, $text) {
+        $n_tabs = (int) $n_tabs;
+        //
+        // C++ specific error.
+        //
+        fwrite($this->outfd, tab($n_tabs) . "#line {$this->LINE_NUMBER} \"{$this->INPUT}\" \n");
+        fwrite($this->outfd, tab($n_tabs) . '#error '. $text . "\n");
     }
     
     public function execute() {
