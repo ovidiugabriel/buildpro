7726614f9d47e81bb2d34672defabc1651a94142
ovidiugabriel <ovidiugabriel@gmail.com>
2016-04-05 18:55:01 +0300

    Create closure-compiler.py

---
+++ src/compiler/closure-compiler.py
@@ -0,0 +1,5 @@
+# https://developers.google.com/closure/compiler/docs/gettingstarted_app
+#
+# The Closure Compiler application is a Java command-line utility that compresses, 
+# optimizes, and looks for mistakes in your JavaScript.
+#
