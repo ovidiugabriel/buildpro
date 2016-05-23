7830b5c92d020700b846981f1e9336b5e7984e91
ovidiugabriel <ovidiugabriel@gmail.com>
2016-05-23 22:55:34 +0300

    Create hacklang.py

---
+++ src/compiler/hacklang.py
@@ -0,0 +1,6 @@
+#
+# Hack is a programming language for HHVM. Hack reconciles the fast 
+# development cycle of a dynamically typed language with the discipline 
+# provided by static typing, while adding many features commonly found in 
+# other modern programming languages.
+#
