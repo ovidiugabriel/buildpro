c905784cdea4156ddf08c16289eac5da004b62ac
Ovidiu <ovidiugabriel@gmail.com>
2016-07-07 17:39:05 +0300

    Update test-xexpr.sh

--- preprocessor/test-xexpr.sh
+++ preprocessor/test-xexpr.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-php xexpr.php in.s > p.out
-racket xexpr.rkt in.s | xmllint --format - > r.out
-LINES=$(diff -u --suppress-common-lines p.out r.out | grep -v ^@ | wc -l)
-LINES=$(($LINES - 2))
+echo "[*] Running PHP ..."
+php xexpr.php in.s | xmllint --format - > p.out
 
-echo $LINES
+echo "[*] Running Racket ..."
+racket xexpr.rkt in.s | xmllint --format - > r.out
+
+LINES=$(diff -u --suppress-common-lines p.out r.out | grep -v ^@ | wc -l)
+if [ "0" != "$LINES" ] ; then
+  LINES=$(($LINES - 2))
+
+  echo "$LINES line(s)"
+
+  diff -u p.out r.out
+fi
