c3ec52e16152776cf1fc79f7e5fa33896e706d0e
Ovidiu <ovidiugabriel@gmail.com>
2016-09-05 14:29:54 +0300

    Update prototyping.py

--- src/prototyping.py
+++ src/prototyping.py
@@ -1,5 +1,9 @@
 
+# from __future__ imports must occur at the beginning of the file
+from __future__ import print_function
 import re
+import sys
+
 
 #
 # phpDocumentor style tags like @param or @return
@@ -12,24 +16,59 @@ phpdoc_hx_types = {
     'boolean': 'Bool',
     'string' : 'String',
     'float'  : 'Float',
-    'int'    : 'Int'
+    'int'    : 'Int',
+    'void'   : 'Void'
 }
 
-def proto_tags(inp):
+def parse_tags(inp, functions):
     matches = re.findall(r"\/\*\*(.*?)\*\/(.*?)function\s+(.*?)\((.*?)\)", inp, re.MULTILINE | re.DOTALL)
     for match in matches:
-        params = re.findall("@param\s+(.*?)\s+\$(.*?)\s", match[0])
-        sig = []
-        for param in params:
-            sig.append(param[1] + ':'+ phpdoc_hx_types[param[0]])
-        returns = re.findall("@return\s+(.*?)\s", match[0])
+        proto_m = re.findall("(@proto\s+.*)", match[0])
+        # print(proto_m)
+        if len(proto_m) > 0:
+            proto_line(proto_m[0], functions)
+        else:
+            params = re.findall("@param\s+(.*?)\s+\$(.*?)\s", match[0])
+            sig = []
+            for param in params:
+                sig.append(param[1] + ':'+ phpdoc_hx_types[param[0]])
+            returns = re.findall("@return\s+(.*?)\s", match[0])
 
-        out = "{specifiers} function {funcname}({params}):{returns};".format(specifiers=match[1].strip(),
-                funcname=match[2],
-                params=', '.join(sig),
-                returns=phpdoc_hx_types[returns[0]]
-            )
-        print(out)
+            proto_str = "{visibility} function {funcname}({params}):{returns};".format(visibility=match[1].strip(),
+                    funcname=match[2],
+                    params=', '.join(sig),
+                    returns=phpdoc_hx_types[returns[0]]
+                )
+            functions.append(proto_str)
+
+def proto_line(line, functions):
+    m = re.search('@proto\s+(static|\.?)\s*(public|private|protected|[\+\#\~\-]?)\s*(.*)', line.rstrip())
+    if None != m:
+        static = m.group(1)
+        if '.' == static:
+            static = 'static'
+
+        if static != '':
+            static += ' '
+
+        #
+        # Haxe has no notion of a protected keyword known from Java, C++ and other object-oriented languages.
+        # However, its private behavior is equal to those language's protected behavior, 
+        # so Haxe actually lacks their real private behavior.
+        #
+        visibility = m.group(2)
+        if ('#' == visibility) or ('-' == visibility) or ('~' == visibility) or ('protected' == visibility):
+            visibility = 'private'
+        if '+' == m.group(2):
+            visibility = 'public'
+
+        if visibility != '':
+            visibility += ' '
+
+        proto = m.group(3)
+        functions.append(static + visibility + 'function ' + proto)
+    else:
+        raise Exception("No match for @proto tag")
 
 def proto(buildpro_print, argv):
     if len(argv) < 5:
@@ -48,32 +87,8 @@ def proto(buildpro_print, argv):
     # Read all @proto annotations
     functions = []
     with open(filename, 'r') as fileh:
-        for line in fileh:
-            m = re.search('@proto\s+(static|\.?)\s*(public|private|protected|[\+\#\~\-]?)\s*(.*)', line.rstrip())
-            if None != m:
-                static = m.group(1)
-                if '.' == static:
-                    static = 'static'
-
-                if static != '':
-                    static += ' '
-
-                #
-                # Haxe has no notion of a protected keyword known from Java, C++ and other object-oriented languages.
-                # However, its private behavior is equal to those language's protected behavior, 
-                # so Haxe actually lacks their real private behavior.
-                #
-                visibility = m.group(2)
-                if ('#' == visibility) or ('-' == visibility) or ('~' == visibility) or ('protected' == visibility):
-                    visibility = 'private'
-                if '+' == m.group(2):
-                    visibility = 'public'
-
-                if visibility != '':
-                    visibility += ' '
-
-                proto = m.group(3)
-                functions.append(static + visibility + 'function ' + proto)
+        content = fileh.read()
+        parse_tags(content, functions)
 
     outfd = open(outfile, 'w')
 
@@ -86,3 +101,11 @@ def proto(buildpro_print, argv):
     outfd.write('} /* end class ' + full_class_name +' */' + "\n")
 
     outfd.close()
+
+if __name__ == "__main__":
+    try:
+        def buildpro_print(x):
+            print(x)
+        proto(buildpro_print, sys.argv)
+    except Exception as ex:
+        exit(int(str(ex)))
