e30a91fbb3e1f032eaf11f5c278c6f755e99da00
Ovidiu <ovidiugabriel@gmail.com>
2016-11-25 13:48:51 +0200

    Update wsd-codegen.php

--- preprocessor/wsd-codegen.php
+++ preprocessor/wsd-codegen.php
@@ -9,32 +9,6 @@
 
 define ('TAB', '    ');
 
-class Node {
-    const TYPE_FINISH = 'finish';
-    const TYPE_RETURN = 'return';
-    const TYPE_START  = 'start';
-    const TYPE_CREATE = 'create';
-    const TYPE_CALL   = 'call';
-
-    public function __construct($type, $from = null, $to = null, $message = null) {
-        if ($type) {
-            $type_names = array(
-                    '-->-'  =>  self::TYPE_FINISH,
-                    '-->'   =>  self::TYPE_RETURN,
-                    '->+'   =>  self::TYPE_START,
-                    '->*'   =>  self::TYPE_CREATE,
-                    '->'    =>  self::TYPE_CALL,
-                );
-            $this->type = isset($type_names[$type]) ? $type_names[$type] : $type;
-        }
-        if ($from) {$this->from = $from;}
-        if ($to) {$this->to = $to;}
-        if ($message) {$this->message = $message;}
-    }
-
-
-}
-
 class DestroyNode extends Node {
     public function __construct($to) {
         parent::__construct('destroy', null, $to, 'destroy');
