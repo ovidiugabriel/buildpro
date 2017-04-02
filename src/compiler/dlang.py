295ceb6f4a627d9118e737994b2f5932b0f8ab95
Ovidiu <ovidiugabriel@gmail.com>
2017-04-02 11:47:39 +0300

    Update dlang.py

--- src/compiler/dlang.py
+++ src/compiler/dlang.py
@@ -2,5 +2,5 @@
 # D is a systems programming language with C-like syntax and static typing. 
 # It combines efficiency, control and modeling power with safety and programmer productivity.
 #
-class dlang(compiler_base):
+class compiler_dlang(compiler_base):
   pass
