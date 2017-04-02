40e517396a08021f04b57d4ca900b7489bd3fb24
Ovidiu <ovidiugabriel@gmail.com>
2017-04-02 11:53:17 +0300

    Update zephir.py

--- src/compiler/zephir.py
+++ src/compiler/zephir.py
@@ -1,3 +1,5 @@
 # Zephir, an open source, high-level language designed 
 # to ease the creation and maintainability of extensions 
 # for PHP with a focus on type and memory safety.
+class compiler_zephir(compiler_base):
+  pass
