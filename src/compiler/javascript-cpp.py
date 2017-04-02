96b4da77ae1601ad87a1aec62866afbf0fdae58d
Ovidiu <ovidiugabriel@gmail.com>
2017-04-02 11:56:29 +0300

    Update javascript-cpp.py

--- src/compiler/javascript-cpp.py
+++ src/compiler/javascript-cpp.py
@@ -1,3 +1,8 @@
 #
-# https://rreverser.com/es6-modules-are-dead-long-live-c-preprocessor/
+# Preprocess your JavaScript files using CPP
 #
+# https://rreverser.com/es6-modules-are-dead-long-live-c-preprocessor/
+# http://www.julienlecomte.net/blog/2007/09/16/
+#
+class compiler_javascript_cpp(compiler_base):
+  pass
