d9007f95c49df67e38815fc1a89ec3c797757b13
Ovidiu <ovidiugabriel@gmail.com>
2017-06-06 12:22:26 +0300

    Create loci.py

---
+++ src/compiler/loci.py
@@ -0,0 +1,10 @@
+
+#
+# Loci it's a language that aims to not only fix many of the problems that plague C++, 
+# but also to introduce whole new paradigms and programming styles that are extremely useful for developers,
+# like: Algebraic Datatypes, Transitive Const, etc.
+#
+# http://loci-lang.org
+#
+class compiler_loci(compiler_base):
+    pass
