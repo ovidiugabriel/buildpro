60d6be01016c0a60bd3cf953e0886c02a64b5d06
Ovidiu <ovidiugabriel@gmail.com>
2017-12-20 15:05:09 +0200

    Create type-size.rkt

---
+++ preprocessor/lib/safe/type-size.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(struct symbol (varname type) #:inspector #f)
+(struct range (min max step) #:inspector #f)
+(struct type (byte-count range) #:inspector #f)
+
+;;
+;; gives the number of bytes used to store `expr`
+;;
+(define/contract (byte-count expr)
+  (->i ([expr symbol?])
+       [result integer?])  
+  (type-byte-count (symbol-type expr)) )
+
+(define uint8_t  (type 1 (range 0 255 1)))
+(define uint16_t (type 2 (range 0 65535 1)))
+(define uint32_t (type 4 (range 0 4294967295 1)))
+(define uint64_t (type 8 (range 0 18446744073709551615 1)))
+
+(define int8_t   (type 1 (range -128 127 1)))
+(define int16_t  (type 2 (range -32768 32767 1)))
+(define int32_t  (type 4 (range -2147483648 2147483647 1)))
+(define int64_t  (type 8 (range -9223372036854775808 9223372036854775807 1)))
+
