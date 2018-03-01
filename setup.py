0a95b9ceb55de00bbde49281efc272b3cc033132
Ovidiu <ovidiugabriel@gmail.com>
2018-03-01 23:49:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ import os
 buildpro_home = os.path.dirname(os.path.realpath(__file__)).replace('\\', '\\\\')
 with open('buildpro', 'w') as file:
     file.write('#!/bin/bash\n')
-    file.write('/bin/env python '+buildpro_home+'/src/buildpro.py $*\n')
+    file.write('export PYTHONPATH=' + buildpro_home + '/PyYAML-3.11/lib')
+    file.write('python ' + buildpro_home + '/src/buildpro.py $*\n')
 
-with open('buildpro.bat', 'w') as file:
-    file.write('@echo off\n')
-    file.write('python '+buildpro_home+'/src/buildpro.py %*\n')
+# .bat files are temporary not in use, please use git-bash for windows
+# with open('buildpro.bat', 'w') as file:
+#    file.write('@echo off\n')
+#    file.write('python '+buildpro_home+'/src/buildpro.py %*\n')
