5dfd78ccc654b3078dfdf37c0d51a519caec7f4d
Ovidiu <ovidiugabriel@gmail.com>
2018-08-02 15:13:59 +0300

    Create crystal.py

---
+++ src/compiler/crystal.py
@@ -0,0 +1,9 @@
+
+# Crystal’s syntax is heavily inspired by Ruby’s, so it feels natural to read 
+# and easy to write, and has the added benefit of a lower learning curve for 
+# experienced Ruby devs.
+#
+# https://crystal-lang.org
+#
+class compiler_crystal(compiler_base):
+  pass
