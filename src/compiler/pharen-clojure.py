72de4ebce37188b316eff1b7068cbd1eb3e52542
Ovidiu <ovidiugabriel@gmail.com>
2018-08-02 15:14:58 +0300

    Create pharen-clojure.py

---
+++ src/compiler/pharen-clojure.py
@@ -0,0 +1,7 @@
+#
+# Plugin for Clojure language frontend for Pharen Runtime
+#
+# Home of Pharen Runtime: https://github.com/scriptor/pharen
+#
+class compiler_pharen_clojure(compiler_base):
+  pass
