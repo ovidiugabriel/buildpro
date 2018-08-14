2f5189460ab5157abe4b2d3dd28479e40076dfd5
Ovidiu <ovidiugabriel@gmail.com>
2018-08-14 15:14:09 +0300

    Create asciidoctor.py

---
+++ src/compiler/asciidoctor.py
@@ -0,0 +1,8 @@
+# Asciidoctor is a fast text processor and publishing toolchain for converting 
+# AsciiDoc content to HTML5, DocBook, PDF, and other formats. Asciidoctor is
+# written in Ruby
+#
+# https://asciidoctor.org
+#
+class compiler_asciidoctor(compiler_base):
+  pass
