a844a67890611dab542c9174fccb81515c4fbfb7
Ovidiu <ovidiugabriel@gmail.com>
2019-05-08 08:33:40 +0300

    Create compile_commands.py

---
+++ src/compile_commands.py
@@ -0,0 +1,16 @@
+##
+## Module used to generate compile_commands.json
+## Documentation: https://clang.llvm.org/docs/JSONCompilationDatabase.html
+##
+## [{
+##    "directory": "string,required",
+##    "file": "string,required",
+##    "command": "string,required",
+##    
+##    "arguments": ["Either", "arguments", "or", "command", "is", "required"],
+##    "output": "string:optional"
+## }, ...]
+##
+
+class compile_commands:
+    pass
