c8da64c463dd6de94cb26a4a44ddcfdd1f6480b8
Ovidiu <ovidiugabriel@gmail.com>
2019-05-18 11:38:19 +0300

    Update zephir.py

--- src/compiler/zephir.py
+++ src/compiler/zephir.py
@@ -1,5 +1,12 @@
-# Zephir, an open source, high-level language designed 
-# to ease the creation and maintainability of extensions 
-# for PHP with a focus on type and memory safety.
+#
+# Zephir - (Zend Engine Php Intrmediate), - is a high level Programming Language
+# that eases the creation and maintainability of extensions for PHP. Zephir 
+# extensions are exported to C code that can be compiled and optimized by major 
+# C compilers such as gcc/clang/vc++. Functionality is exposed to the PHP
+# language.
+#
+# Zephir, an open source, high-level language designed to ease the creation and 
+# maintainability of extensions for PHP with a focus on type and memory safety.
+# 
 class compiler_zephir(compiler_base):
   pass
